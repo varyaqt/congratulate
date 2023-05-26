5b9aba0fd7fbf29ca607994049d8e783784d885a
Alexander <alykoshin@users.noreply.github.com>
2023-05-26 03:16:47 +0300

    add sendData(), refactor, improve readme

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
 import React from "react";
-import {
-  createSmartappDebugger,
-  createAssistant,
-} from "@salutejs/client";
-//} from "@sberdevices/assistant-client";
+import {createAssistant, createSmartappDebugger,} from "@salutejs/client";
 
 import "./App.css";
-import { TaskList } from './pages/TaskList';
+import {TaskList} from './pages/TaskList';
 
 
 const initializeAssistant = (getState/*: any*/) => {
@@ -17,9 +13,8 @@ const initializeAssistant = (getState/*: any*/) => {
       getState,
     });
   }
-  return createAssistant({ getState });
+  return createAssistant({getState});
 };
-
 
 
 export class App extends React.Component {
@@ -32,14 +27,37 @@ export class App extends React.Component {
       notes: [],
     }
 
-    this.assistant = initializeAssistant(() => this.getStateForAssistant() );
+    this.assistant = initializeAssistant(() => this.getStateForAssistant());
+
     this.assistant.on("data", (event/*: any*/) => {
       console.log(`assistant.on(data)`, event);
-      const { action } = event
-      this.dispatchAssistantAction(action);
+      if (event.type === "character") {
+        console.log(`assistant.on(data): character: "${event?.character?.id}"`);
+      } else if (event.type === "insets") {
+        console.log(`assistant.on(data): insets`);
+      } else {
+        const {action} = event;
+        this.dispatchAssistantAction(action);
+      }
     });
+
     this.assistant.on("start", (event) => {
-      console.log(`assistant.on(start)`, event);
+
+      let initialData = this.assistant.getInitialData();
+
+      console.log(`assistant.on(start)`, event, initialData);
+    });
+
+    this.assistant.on("command", (event) => {
+      console.log(`assistant.on(command)`, event);
+    });
+
+    this.assistant.on("error", (event) => {
+      console.log(`assistant.on(error)`, event);
+    });
+
+    this.assistant.on("tts", (event) => {
+      console.log(`assistant.on(tts)`, event);
     });
 
   }
@@ -48,12 +66,12 @@ export class App extends React.Component {
     console.log('componentDidMount');
   }
 
-  getStateForAssistant () {
+  getStateForAssistant() {
     console.log('getStateForAssistant: this.state:', this.state)
     const state = {
       item_selector: {
         items: this.state.notes.map(
-          ({ id, title }, index) => ({
+          ({id, title}, index) => ({
             number: index + 1,
             id,
             title,
@@ -65,7 +83,7 @@ export class App extends React.Component {
     return state;
   }
 
-  dispatchAssistantAction (action) {
+  dispatchAssistantAction(action) {
     console.log('dispatchAssistantAction', action);
     if (action) {
       switch (action.type) {
@@ -84,46 +102,80 @@ export class App extends React.Component {
     }
   }
 
-  add_note (action) {
+  add_note(action) {
     console.log('add_note', action);
     this.setState({
       notes: [
         ...this.state.notes,
         {
-          id:        Math.random().toString(36).substring(7),
-          title:     action.note,
+          id: Math.random().toString(36).substring(7),
+          title: action.note,
           completed: false,
         },
       ],
     })
   }
 
-  done_note (action) {
+  done_note(action) {
     console.log('done_note', action);
     this.setState({
       notes: this.state.notes.map((note) =>
         (note.id === action.id)
-        ? { ...note, completed: !note.completed }
-        : note
+          ? {...note, completed: !note.completed}
+          : note
       ),
     })
   }
 
-  delete_note (action) {
+  _send_action_value(action_id, value) {
+    const data = {
+      action: {
+        action_id: action_id,
+        parameters: {   // значение поля parameters может любым, но должно соответствовать серверной логике
+          value: value, // см.файл src/sc/noteDone.sc смартаппа в Studio Code
+        }
+      }
+    };
+    const unsubscribe = this.assistant.sendData(
+      data,
+      (data) => {   // функция, вызываемая, если на sendData() был отправлен ответ
+        const {type, payload} = data;
+        console.log('sendData onData:', type, payload);
+        unsubscribe();
+      });
+  }
+
+  play_done_note(id) {
+    const completed = this.state.notes.find(({id}) => id)?.completed;
+    if (!completed) {
+      const texts = ['Молодец!', 'Красавчик!', 'Супер!'];
+      const idx = Math.random() * texts.length | 0;
+      this._send_action_value('done', texts[idx]);
+    }
+  }
+
+  delete_note(action) {
     console.log('delete_note', action);
     this.setState({
-      notes: this.state.notes.filter(({ id }) => id !== action.id),
+      notes: this.state.notes.filter(({id}) => id !== action.id),
     })
   }
 
   render() {
     console.log('render');
     return (
-      <TaskList
-        items  = {this.state.notes}
-        onAdd  = {(note) => { this.add_note({ type: "add_note", note }); }}
-        onDone = {(note) => { this.done_note({ type: "done_note", id: note.id }) }}
-      />
+      <>
+        <TaskList
+          items={this.state.notes}
+          onAdd={(note) => {
+            this.add_note({type: "add_note", note});
+          }}
+          onDone={(note) => {
+            this.play_done_note(note.id);
+            this.done_note({type: "done_note", id: note.id});
+          }}
+        />
+      </>
     )
   }
 
